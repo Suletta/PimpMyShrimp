3171b99f0ff8310d0cb115dfd4926196008c3a51
Suletta <60387103+Suletta@users.noreply.github.com>
2021-04-16 17:51:56 +0200

    Main menu placeholder
    Abbozzato il main menu, wip menu in game

---
+++ Assets/Sula/Script/GameMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject gameMenu;
+
+    void Start()
+    {
+        gameMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenMenu()
+    {
+        gameMenu.SetActive(false);
+    }
+}
---
+++ Assets/Sula/Script/PlayerInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerInput : MonoBehaviour
+{
+    public InputAction OpenMenu;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        OpenMenu.Enable();   
+    }
+    private void OnDisable()
+    {
+        OpenMenu.Disable();
+    }
+}
