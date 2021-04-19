02eda2b4f61d93902618c1366782b12467e11e0f
RickSera <77831050+RickSera@users.noreply.github.com>
2021-04-19 14:48:20 +0200

    19/04 15:00
    Spawer e Bullet wip

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //public Vector2 velocity;
+    public float speed;
+    [HideInInspector]public float rotation;
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(0, rotation, 0);
+    }
+    void Update()
+    {
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Debug.Log("a");
+    }
+}
---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public Bullet bullet;
+    public float minRotation, maxRotation, rate, bulletSpeed;
+    public int numberOfBullet;
+    public Vector2 bulletVelocity;
+
+    private float timer = 0;
+    public float[] rotations ;
+
+    private void Update()
+    {
+        SetRotations();
+        if(timer >= rate)
+        {
+            SpawnBullet();
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+
+    private float[] SetRotations()
+    {
+        for (int i = 0; i < numberOfBullet; i++)
+        {
+            var fraction = (float)i / (float)numberOfBullet;
+            var difference = maxRotation - minRotation;
+            var fdd = fraction * difference;
+            rotations[i] = fdd + minRotation;
+        }
+        return rotations;
+    }
+
+
+    private void SpawnBullet()
+    {
+        for (int i = 0; i < numberOfBullet; i++)
+        {
+            Bullet b = Instantiate(bullet,new Vector3(), new Quaternion() );
+            b.speed = bulletSpeed;
+            b.rotation = rotations[i];
+            Destroy(b.gameObject, 10);
+        }
+    }
+}
