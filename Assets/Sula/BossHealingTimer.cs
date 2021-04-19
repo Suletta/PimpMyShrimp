44baea78e83a7e524070fadf72bb3a5bc0e4ff48
Suletta <60387103+Suletta@users.noreply.github.com>
2021-04-19 22:32:25 +0200

    Impostando i Metodi di aggiornamento UI

---
+++ Assets/Sula/BossHealingTimer.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+
+public class BossHealingTimer : MonoBehaviour
+{
+    //[SerializeField]
+    //GameObject timerObj;
+    //TextMeshPro timerText;
+
+    //float timerDuration = 10f; //10 sec di timer
+    //bool countingDown = false;
+
+
+    //void Start()
+    //{
+    //    timerText = timerObj.GetComponent<TextMeshPro>();
+    //    timerObj.GetComponent<TextMeshPro>().text = timerDuration + " seconds";
+    //}
+
+    //void Update()
+    //{
+    //    if (countingDown == false && timerDuration > 0)
+    //    {
+    //        StartCoroutine("Countdown");
+    //    }
+
+    //}
+
+    //public IEnumerator Countdown()
+    //{
+    //    Debug.Log("parte il timer");
+    //    countingDown = true;
+
+    //    yield return new WaitForSeconds(1);
+
+    //    timerDuration -= 1;
+
+    //    timerText.text = timerDuration + " seconds";
+    //}
+
+
+    [SerializeField]
+    bool activateTimer = false;
+
+    float currentTime;
+    public int startSeconds;
+
+    //text stuff
+    public GameObject textObj;
+    TextMeshProUGUI textTMP;
+
+    private void Awake()
+    {
+        textTMP = textObj.GetComponent<TextMeshProUGUI>();
+        currentTime = startSeconds;
+    }
+
+    private void Update()
+    {     
+        ActivateCountdown();
+    }
+
+    private void ActivateCountdown()
+    {
+        if (activateTimer)
+        {
+            textTMP.text = currentTime.ToString("F2") + " seconds";
+
+            if (currentTime > 0)
+            {
+                currentTime -= Time.deltaTime;
+            }
+            else
+            {
+                activateTimer = false;
+                currentTime = startSeconds;
+            }
+
+            
+        }
+    }
+
+
+}
---
+++ Assets/UpdatePlayerStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatePlayerStats : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] playerHearts;
+
+    [SerializeField]
+    GameObject playerDamage;
+
+    [SerializeField]
+    GameObject[] playerPowerUp;
+
+
+
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
