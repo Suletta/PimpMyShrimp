1ef33cea1dedcd3c94ad39b5fc8027b0191d9c90
RickSera <77831050+RickSera@users.noreply.github.com>
2021-04-20 12:50:56 +0200

    20/4 13:00

--- Assets/Ric/Scripts/BulletSpawner.cs
+++ Assets/Ric/Scripts/BulletSpawner.cs
@@ -43,7 +43,7 @@ public class BulletSpawner : MonoBehaviour
     {
         for (int i = 0; i < numberOfBullet; i++)
         {
-            Bullet b = Instantiate(bullet,new Vector3(), Quaternion.Euler (0,0,0 ), transform );
+            Bullet b = Instantiate(bullet,new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.Euler (0,0,0 ), transform );
             b.speed = bulletSpeed;
             b.rotation = rotations[i];
             Destroy(b.gameObject, 10);
