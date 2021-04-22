2dcf3601f5c72dd5145bb8ad11354bf0db5c5515
Suletta <60387103+Suletta@users.noreply.github.com>
2021-04-22 10:26:51 +0200

    UI Pronta (mancano da rifinire 2 cose)
    Rifinire dimensioni font e mettere icone dei comandi e inserire immagini mancanti

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -3,18 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
-    [SerializeField] AudioMixer audioMixer;
-    [SerializeField] Dropdown resDropdown;
+    [SerializeField] AudioMixer audioMixer;  
+    [SerializeField] TMP_Dropdown resDropdown;
     Resolution[] resolutions;
 
     private void Start()
     {
-        resolutions = Screen.resolutions;
+        GetResolution();
+
     }
 
+    private void GetResolution()
+    {
+        resolutions = Screen.resolutions;
+        resDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width 
+                && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resDropdown.AddOptions(options);
+        resDropdown.value = currentResolutionIndex;
+        resDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width,resolution.height, Screen.fullScreen);
+    }
 
     public void SetVolume(float volume)
     {
