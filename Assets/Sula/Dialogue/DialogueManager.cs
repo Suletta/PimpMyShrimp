e847dde690062aaef1e73d0fa9d63679320991a9
Suletta <60387103+Suletta@users.noreply.github.com>
2021-04-22 12:09:56 +0200

    Aggiunto audio
    Sempre rifinire dimensioni font e mettere icone dei comandi e inserire immagini mancanti
    WIP DIALOGHI

---
+++ Assets/Sula/Dialogue/DialogueManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    Queue<string> sentences;
+
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    
+}
